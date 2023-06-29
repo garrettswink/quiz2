9d9eaa8f3ae6c9a616f949fb97da491fa15d8d6b
Garrett <garrettswink421@gmail.com>
2023-06-28 22:54:37 -0400

    First Submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 
-// Variables List
+//Variables List
 var startButton = document.getElementById('startButton');
 var quizContainer = document.getElementById('quizContainer');
 var question = document.getElementById('question');
@@ -13,6 +13,66 @@ var buttonB = document.getElementById('buttonB');
 var buttonC = document.getElementById('buttonC');
 var buttonD = document.getElementById('buttonD');
 var nextButton = document.getElementById('nextButton')
+
+
+//Questions & Answers
+const quizArray = [
+    {
+        id: "0",
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Language",
+            "Historic Task Meneuvering Lexicon",
+            "Hard Terminal Management Layer",
+            "It doesn't stand for anything...like KFC."
+        ],
+        correct: "Hyper Text Markup Language",
+    },
+    {
+        id: "1",
+        question: "What does CSS stand for?",
+        options: [
+            "Cat Supervision Strategies",
+            "Cascading Style Sheets",
+            "Crystal Solvent Solution",
+            "Clairvoyant Search Selectors",
+        ],
+        correct: "Cascading Style Sheets",
+    },
+    {
+        id: "2",
+        question: "Which language adds interactivity to a website?",
+        options: [
+            "HTML",
+            "CSS",
+            "French",
+            "JavaScript",
+        ],
+        correct: "JavaScript",
+    },
+    {
+        id: "3",
+        question: "Which of the following is truthy",
+        options: [
+            "If X than Y",
+            "0",
+            "False",
+            "Mac is better than PC",
+        ],
+        correct: '0',
+    },
+    {
+        id: "4",
+        question: "A JavaScript variable is _____",
+        options: [
+            "The mysterious force that animates gif files",
+            "JavaScript doesn't use variables",
+            "A container that holds a value",
+            "The part of the code that does the thing",
+        ],
+        correct: 'A container that holds a value',
+    },
+];
 
 //Click to Start the Quiz
 document.getElementById("startButton").addEventListener("click", function() {
@@ -28,49 +88,6 @@ document.getElementById("startButton").addEventListener("click", function() {
     buttonD.style.display = 'block';
     buttonD.innerHTML = 'It does not stand for anything';
 });
-
-// Answer Button Actions
-// Correct Answer
-buttonA.addEventListener('click', function(){
-    buttonA.style.backgroundColor = "green";
-    nextButton.style.display = 'block';
-});
-
-// Incorrect Answers
-buttonB.addEventListener('click', function(){
-    buttonB.style.backgroundColor = "red";
-    nextButton.style.display = 'block';
-});
-buttonC.addEventListener('click', function(){
-    buttonC.style.backgroundColor = "red";
-    nextButton.style.display = 'block';
-});
-buttonD.addEventListener('click', function(){
-    buttonD.style.backgroundColor = "red";
-    nextButton.style.display = 'block';
-});
-
-// Next Button Action
-nextButton.addEventListener('click',function () {
-    nextButton.style.display = "none";
-    question.innerHTML = "What does CSS stand for?";
-
-    buttonA.style.display = 'block';
-    buttonA.innerHTML = 'Cat Supervision Strategies';
-    buttonB.style.display = 'block';
-    buttonB.innerHTML = 'Cascading Style Sheets';
-    buttonC.style.display = 'block';
-    buttonC.innerHTML = 'Crystal Solvent Solution';
-    buttonD.style.display = 'block';
-    buttonD.innerHTML = 'Clairvoyan Search Selectors';
-});
-
-buttonA.addEventListener('click', function(){
-    buttonA.style.backgroundColor = "red";
-    nextButton.style.display = 'block';
-
-});
-
 
 // Quiz Questions
 const quizArray = [
@@ -130,6 +147,51 @@ const quizArray = [
         correct: 'A container that holds a value',
     },
 ];
+
+// Answer Button Actions
+// Correct Answer
+buttonA.addEventListener('click', function(){
+    buttonA.style.backgroundColor = "green";
+    nextButton.style.display = 'block';
+});
+
+// Incorrect Answers
+buttonB.addEventListener('click', function(){
+    buttonB.style.backgroundColor = "red";
+    nextButton.style.display = 'block';
+});
+buttonC.addEventListener('click', function(){
+    buttonC.style.backgroundColor = "red";
+    nextButton.style.display = 'block';
+});
+buttonD.addEventListener('click', function(){
+    buttonD.style.backgroundColor = "red";
+    nextButton.style.display = 'block';
+});
+
+// Next Button Action
+nextButton.addEventListener('click',function () {
+    nextButton.style.display = "none";
+    question.innerHTML = "What does CSS stand for?";
+
+    buttonA.style.display = 'block';
+    buttonA.innerHTML = 'Cat Supervision Strategies';
+    buttonB.style.display = 'block';
+    buttonB.innerHTML = 'Cascading Style Sheets';
+    buttonC.style.display = 'block';
+    buttonC.innerHTML = 'Crystal Solvent Solution';
+    buttonD.style.display = 'block';
+    buttonD.innerHTML = 'Clairvoyan Search Selectors';
+});
+
+buttonA.addEventListener('click', function(){
+    buttonA.style.backgroundColor = "red";
+    nextButton.style.display = 'block';
+
+});
+
+
+
 
 //Quiz Loop
 for (var i = 0; i < quizArray.length; i++) {
